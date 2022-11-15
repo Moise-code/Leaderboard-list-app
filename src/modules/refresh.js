c62e508c1c650e61d8ac2de72cda896e5dc5fbf2
Moise-code <moisewowe@gmail.com>
2022-11-15 11:47:03 +0200

    clean up comments

--- src/modules/refresh.js
+++ src/modules/refresh.js
@@ -1,12 +1,3 @@
-// import { refresh, scoresName } from './newScore.js';
-
-// const refreshPage = () => {
-//   refresh.addEventListener('click', () => {
-//     scoresName.innerHTML = '';
-//   });
-// };
-
-// export default refreshPage;
 import { refresh, scoreName } from './newScore';
 const refreshPageList = ()=>{
   refresh.addEventListner('click', () =>{
